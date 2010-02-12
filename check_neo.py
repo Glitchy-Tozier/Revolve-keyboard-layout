34ad9b715fc02bdd1218995492c92414b6a0433e
arne <arne@brand.medma>
2010-02-12 18:33:19 +0100

    Changed the cost function

--- check_neo.py
+++ check_neo.py
@@ -192,11 +192,19 @@ QWERTZ_LAYOUT = [
     [(), (), (), (" "), (), (), (), ()] # Reihe 4 mit Leertaste
 ]
 
-COST_PER_KEY  = [ # 0 heißt nicht beachtet
+COST_PER_KEY_OLD  = [ # 0 heißt nicht beachtet
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Zahlenreihe (0)
         [0,6,3,3,3,4,4,3,3,3,6,7,8,0], # Reihe 1
         [0,2,1,1,1,3,3,1,1,1,2,6,0,9], # Reihe 2
         [0,4,5,5,5,5,7,7,5,5,5,5,0],     # Reihe 3
+        [0,0,0,     9     ,0,0,0,0] # Reihe 4 mit Leertaste
+]
+
+COST_PER_KEY  = [ # 0 heißt nicht beachtet
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Zahlenreihe (0)
+        [0,6,3,3,3,4,4,3,3,3,6,7,8,0], # Reihe 1
+        [0,3,2,2,1,3,3,1,2,2,3,6,0,9], # Reihe 2
+        [0,5,5,5,5,5,7,7,5,5,5,5,0],     # Reihe 3
         [0,0,0,     9     ,0,0,0,0] # Reihe 4 mit Leertaste
 ]
 
