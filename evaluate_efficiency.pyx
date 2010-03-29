c79f712b2bfb65179c1b9610b0755006a2ec6820
arne <arne@brand.medma>
2010-03-29 16:37:11 +0200

    use logscale

--- evaluate_efficiency.pyx
+++ evaluate_efficiency.pyx
@@ -1,5 +1,6 @@
 set width 15
+set logscale x1
 plot "2010-03-29-evolve-range-final-penalty.txt" title "final penalty in billion points", "2010-03-29-evolve-range-finger-repeats.txt" title "Finger repeats in percent", "2010-03-29-evolve-range-finger-disbalance.txt" title "Finger disbalance in pseudo standard deviation", "2010-03-29-evolve-range-key-position.txt" title "key position cost"
 f(x) = a * b**(x*c) + d
 fit f() "2010-03-29-evolve-range-final-penalty.txt" using 1:2 via a,b,c,d
-plot "2010-03-29-evolve-range-final-penalty.txt" title "final penalty in billion points", "2010-03-29-evolve-range-finger-repeats.txt" title "Finger repeats in percent", "2010-03-29-evolve-range-finger-disbalance.txt" title "Finger disbalance in pseudo standard deviation", "2010-03-29-evolve-range-key-position.txt" title "key position cost", f(x) title "final penalty fit $a \cdot b^{x \cdot c} + d$"
+plot "2010-03-29-evolve-range-final-penalty.txt" title "final penalty in billion points", "2010-03-29-evolve-range-finger-repeats.txt" title "Finger repeats in percent", "2010-03-29-evolve-range-finger-disbalance.txt" title "Finger disbalance in pseudo standard deviation", "2010-03-29-evolve-range-key-position.txt" title "key position cost", f(x) title "final penalty fit $a \cdot b^{x \cdot c} + d$", 1.380194974 title "neo final penalty", 2.048361472 title "Qwertz final penalty"
