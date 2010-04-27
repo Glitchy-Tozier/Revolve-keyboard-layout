f5945c9d70a961a0a0f3766a4954998416240c62
Arne Babenhauserheide <bab@draketo.de>
2010-04-27 23:09:29 +0200

    Nicer evaluation.

--- evaluate_efficiency.pyx
+++ evaluate_efficiency.pyx
@@ -1,10 +1,17 @@
 set width 15
 set logscale x1
+
+set title "Aggregate results"
 plot "results/2010-04-27-evolve-range-final-penalty.txt" title "final penalty in billion points", "results/2010-04-27-evolve-range-finger-repeats.txt" title "Finger repeats in percent", "results/2010-04-27-evolve-range-finger-disbalance.txt" title "Finger disbalance in pseudo standard deviation", "results/2010-04-27-evolve-range-key-position.txt" title "key position cost"
-# f(x) = a * b**(x*c) + d
-# fit f() "results/2010-04-27-evolve-range-final-penalty.txt" using 1:2 via a,b,c,d
-plot "results/2010-04-27-evolve-range-final-penalty.txt" title "final penalty in billion points", "results/2010-04-27-evolve-range-finger-repeats.txt" title "Finger repeats in percent", "results/2010-04-27-evolve-range-finger-disbalance.txt" title "Finger disbalance in pseudo standard deviation", "results/2010-04-27-evolve-range-key-position.txt" title "key position cost", 1.811033032 title "neo final penalty", 2.830310786 title "Qwertz final penalty"
-plot "results/2010-04-27-evolve-range-final-penalty.txt" title "final penalty in billion points", 2.830310786 title "Qwertz final penalty", 1.811033032 title "neo final penalty"
+
+set title "Final penalty"
+plot "results/2010-04-27-evolve-range-final-penalty.txt" title "final penalty in billion points", 1.998716743 title "Neo", 3.055235076 title "Qwertz", 1.736400007 title "NordTast"
+
+set title "Finger repeats"
 plot "results/2010-04-27-evolve-range-finger-repeats.txt" title "Finger repeats [percent]", 4.82198888113 title "Neo", 6.72632774411 title "Qwertz", 1.83742456166 title "Nordtast"
-plot "results/2010-04-27-evolve-range-finger-disbalance.txt" title "finger disbalance [million keystrokes]", 7.75577431662 title "Qwertz finger disbalance [million keystrokes]", 4.56473130298 title "Neo finger disbalance [million keystrokes]"
-plot "results/2010-04-27-evolve-range-key-position.txt" title "mean key position cost", 5.53615632047 title "Qwertz mean key position cost", 4.06408223929 title "Neo mean key position cost"
+
+set title "Finger disbalance in pseudo standard deviation"
+plot "results/2010-04-27-evolve-range-finger-disbalance.txt" title "finger disbalance [million keystrokes]", 9.84247826956 title "Qwertz", 9.68350392408 title "Neo", 3.75005118331 title "NordTast"
+
+set title "mean key position cost"
+plot "results/2010-04-27-evolve-range-key-position.txt" title "mean key position cost", 5.53617048458 title "Qwertz", 4.06408223929 title "Neo", 3.96412236288 title "NordTast"
