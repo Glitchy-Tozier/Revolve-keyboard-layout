17c519b627f00afe5486af6c1da0c19f0ec42a11
Arne Babenhauserheide <bab@draketo.de>
2010-04-27 23:47:37 +0200

    Added help and general data identifier to the eval script.

--- evaluate_efficiency.sh
+++ evaluate_efficiency.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]; then 
+    echo "usage: $0 <identifier>"
+    exit 0
+fi
+
+if [ "$1" == "--help" ]; then
+    echo "usage: $0 <identifier>"
+    exit 0
+fi
+
 echo create data
-for j in {1..9} {1..9}0 {1..9}00 {1..9}000 ; do time ./check_neo.py --evolve $j --prerandomize 10000 -v -q >> results/2010-04-27-evolve-$j-results.txt; done
+for j in {1..9} {1..9}0 {1..9}00 {1..9}000 ; do time ./check_neo.py --evolve $j --prerandomize 10000 -v -q >> results/$1-evolve-$j-results.txt; done
 
 echo check and plot the data
 echo … final penalty 
-for j in {1..9} {1..9}0 {1..9}00 {1..9}000; do grep "billion total" results/2010-04-27-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done > results/2010-04-27-evolve-range-final-penalty.txt 
+for j in {1..9} {1..9}0 {1..9}00 {1..9}000; do grep "billion total" results/$1-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done > results/$1-evolve-range-final-penalty.txt 
 echo … finger repeats
-for j in {1..9} {1..9}0 {1..9}00 {1..9}000; do grep 2gramme results/2010-04-27-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done > results/2010-04-27-evolve-range-finger-repeats.txt 
+for j in {1..9} {1..9}0 {1..9}00 {1..9}000; do grep 2gramme results/$1-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done > results/$1-evolve-range-finger-repeats.txt 
 echo … finger disbalance
-for j in {1..9} {1..9}0 {1..9}00 {1..9}000; do grep disbalance results/2010-04-27-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done > results/2010-04-27-evolve-range-finger-disbalance.txt
+for j in {1..9} {1..9}0 {1..9}00 {1..9}000; do grep disbalance results/$1-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done > results/$1-evolve-range-finger-disbalance.txt
 echo … key position cost
-for j in {1..9} {1..9}0 {1..9}00 {1..9}000; do grep 1gramme results/2010-04-27-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done > results/2010-04-27-evolve-range-key-position.txt
+for j in {1..9} {1..9}0 {1..9}00 {1..9}000; do grep 1gramme results/$1-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done > results/$1-evolve-range-key-position.txt
 
 # and old style results
-for j in {1..130} 1{4..9}0 {201..205} {301..305} {401..405} {601..603} {1001..1003} 10000; do grep "billion total" results/2010-04-27-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done >> results/2010-04-27-evolve-range-final-penalty.txt; for j in {1..130} 1{4..9}0 {201..205} {301..305} {401..405} {601..603} {1001..1003} 10000; do grep 2gramme results/2010-04-27-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done >> results/2010-04-27-evolve-range-finger-repeats.txt; for j in {1..130} 1{4..9}0 {201..205} {301..305} {401..405} {601..603} {1001..1003} 10000; do grep disbalance results/2010-04-27-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done >> results/2010-04-27-evolve-range-finger-disbalance.txt; for j in {1..130} 1{4..9}0 {201..205} {301..305} {401..405} {601..603} {1001..1003} 10000; do grep 1gramme results/2010-04-27-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done >> results/2010-04-27-evolve-range-key-position.txt
+for j in {1..130} 1{4..9}0 {201..205} {301..305} {401..405} {601..603} {1001..1003} 10000; do grep "billion total" results/$1-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done >> results/$1-evolve-range-final-penalty.txt; for j in {1..130} 1{4..9}0 {201..205} {301..305} {401..405} {601..603} {1001..1003} 10000; do grep 2gramme results/$1-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done >> results/$1-evolve-range-finger-repeats.txt; for j in {1..130} 1{4..9}0 {201..205} {301..305} {401..405} {601..603} {1001..1003} 10000; do grep disbalance results/$1-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done >> results/$1-evolve-range-finger-disbalance.txt; for j in {1..130} 1{4..9}0 {201..205} {301..305} {401..405} {601..603} {1001..1003} 10000; do grep 1gramme results/$1-evolve-$j-results.txt | cut -d " " -f 2| sed "s/^/$j /" ; done >> results/$1-evolve-range-key-position.txt
 echo with pyxplot
 # pyxplot evaluate_efficiency.pyx
