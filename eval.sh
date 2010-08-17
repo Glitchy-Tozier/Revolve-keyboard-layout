bab3e35301367ebc571ef27d909f2e99a4e0835e
Arne Babenhauserheide <bab@draketo.de>
2010-08-17 17:19:26 +0200

    more flexible eval.sh

--- eval.sh
+++ eval.sh
@@ -1,10 +1,23 @@
+#!/bin/bash
+
+PATHPART=results/2010-08-16-
+
+if [ $# -gt 0 ]; then
+    if [ "$1" == "--help" ]; then
+        echo "usage: $0 [<part of the path; gets appended *.txt>]"
+        echo "example: $0 results/2010-08-15"
+        exit 0
+    fi
+    PATHPART=$1
+fi
+
 mkdir -p eval
-grep -h total results/2010-08-16-*txt | cut -d " " -f 2 > eval/total
-grep -h "key position" results/2010-08-16-*txt | ./pyline "line.split('( ')[1].split(' )')[0]" > eval/keys
-grep -h "2gramme" results/2010-08-16-*txt | ./pyline "line.split('( ')[1].split(' )')[0]" > eval/finger-rep
-grep -h "keystrokes" results/2010-08-16-*txt | ./pyline "line.split('( ')[1].split(' )')[0]" > eval/disbalance
-grep -h "unbalancing" results/2010-08-16-*txt | cut -d " " -f 2 > eval/unbalance
-grep -h "trigrams have no hand" results/2010-08-16-*txt | ./pyline "line.split('( ')[1].split(' )')[0]" > eval/handswitch
-grep -h "rows" results/2010-08-16-*txt | ./pyline "line.split('( ')[1].split(' )')[0]" > eval/rows
-grep -h "shortcut" results/2010-08-16-*txt | cut -d " " -f 2 > eval/short
-grep -h "pattern" results/2010-08-16-*txt | cut -d " " -f 2 > eval/pattern
+grep -h total $PATHPART*txt | cut -d " " -f 2 > eval/total
+grep -h "key position" $PATHPART*txt | ./pyline "line.split('( ')[1].split(' )')[0]" > eval/keys
+grep -h "2gramme" $PATHPART*txt | ./pyline "line.split('( ')[1].split(' )')[0]" > eval/finger-rep
+grep -h "keystrokes" $PATHPART*txt | ./pyline "line.split('( ')[1].split(' )')[0]" > eval/disbalance
+grep -h "unbalancing" $PATHPART*txt | cut -d " " -f 2 > eval/unbalance
+grep -h "trigrams have no hand" $PATHPART*txt | ./pyline "line.split('( ')[1].split(' )')[0]" > eval/handswitch
+grep -h "rows" $PATHPART*txt | ./pyline "line.split('( ')[1].split(' )')[0]" > eval/rows
+grep -h "shortcut" $PATHPART*txt | cut -d " " -f 2 > eval/short
+grep -h "pattern" $PATHPART*txt | cut -d " " -f 2 > eval/pattern
