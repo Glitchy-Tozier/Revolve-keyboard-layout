20f23bcce19c95b5427f3a0119cccf64b8146750
Arne Babenhauserheide <bab@draketo.de>
2010-09-14 15:46:34 +0200

    refactoring (make importable).

--- recheck_all_result_layouts.py
+++ recheck_all_result_layouts.py
@@ -2,30 +2,36 @@
 
 """get all layout results from the results folder.
 
-Depends on the layouts info starting with 'Evolved Layout'
+Depends on the layouts info starting with OA'Evolved Layout'
 """
 
 from check_neo import string_to_layout, print_layout_with_statistics
 from os import listdir
 from os.path import join
 
-d = ""
-for i in listdir("results"):
-    if not i.endswith(".txt"):
-        continue
-    with open(join("results", i), encoding="utf-8") as f:
-        try: 
-            d += f.read()
-        except UnicodeError:
-            print("can’t open", i)
+def get_all_layouts_in_text_files_in(folder="results"):
+    """get all layouts from check_neo runs saved in the textfile."""
+    d = ""
+    for i in listdir("results"):
+        if not i.endswith(".txt"):
+            continue
+        with open(join("results", i), encoding="utf-8") as f:
+            try: 
+                d += f.read()
+            except UnicodeError:
+                print("can’t open", i)
 
-e = d.split("Evolved Layout")
-
-layout_strings = []
-for i in e[1:]:
-    layout_strings.append("\n".join(i.splitlines()[1:4]))
+    e = d.split("Evolved Layout")
+    layout_strings = []
+    for i in e[1:]:
+        layout_strings.append("\n".join(i.splitlines()[1:4]))
     
-all_layouts = [string_to_layout(l) for l in layout_strings]
+    all_layouts = [string_to_layout(l) for l in layout_strings]
+    return all_layouts
 
-for lay in all_layouts: 
-    print_layout_with_statistics(lay, verbose=True)
+
+if __name__ == "__main__": 
+    all_layouts = get_all_layouts_in_text_files_in("results")
+    
+    for lay in all_layouts: 
+        print_layout_with_statistics(lay, verbose=True)
