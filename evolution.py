e0886d8bcbc9ffc936c8acce01b3c83fc4cfde42
Arne Babenhauserheide <bab@draketo.de>
2010-10-11 23:27:55 +0200

    evolution quiet again.

--- evolution.py
+++ evolution.py
@@ -23,7 +23,7 @@ prerandomize = 3000
 controlled = False
 
 #: Should we spout out information on the shell? (Windows users disable this. Your shell can’t take Unicode)
-quiet = False
+quiet = True
 
 #: Should we give additional statistics for the final layout?
 verbose = True
