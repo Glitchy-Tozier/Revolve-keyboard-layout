185323b0ee2257fbd77e8f1c7c208e000a679198
Arne Babenhauserheide <bab@draketo.de>
2010-11-30 02:35:27 +0100

    reduced cost of the stretched middle fingers and the low index fingers (very easy to reach).

--- config.py
+++ config.py
@@ -120,9 +120,9 @@ COST_PER_KEY  = [
     # The 0 values aren’t filled in at the moment.
     # Don’t put mutated keys there, otherwise the best keys will end up there!
     [50,    40,35,30,30, 35,   40,35,30,30,30,35,40,50], # Zahlenreihe (0)
-    [24,    20, 6, 5, 6, 9,    10, 5, 4, 5, 8,24,36, 0], # Reihe 1
+    [24,    20, 6, 4, 6, 9,    10, 5, 4, 5, 8,24,36, 0], # Reihe 1
     [12,     3, 3, 3, 3, 5,     5, 3, 3, 3, 3, 5,10,18], # Reihe 2
-    [15,10,12,24,20, 10,   30, 7, 6,22,22,10,   15],     # Reihe 3
+    [15,10, 12,24,20, 5,    30, 6, 5,22,22,10,      15],     # Reihe 3
     [0,0,0,               3           , 0, 0, 0, 0] # Reihe 4 mit Leertaste
 ]
 
