ce6615371c43168f4a5506611aeeacf0fa6ff4c8
Arne Babenhauserheide <bab@draketo.de>
2011-02-28 00:41:53 +0100

    added a script for creating movies from the sgvs in the folder svgs/

---
+++ create_svg_video.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# create a video from the svgs in the folder svgs
+# svgs to pngs
+for i in svgs/*svg; do inkscape -D -z -e  $(basename $i .svg).png -f $i; done
+# create a temporary file with all pngs in reverse order
+ls svgs/*png | sort -r > /tmp/pnglist.txt
+# create the video
+# mencoder "mf://@/tmp/pnglist.txt" -mf fps=10 -o svgs/video.avi -ovc copy
+mencoder "mf://@/tmp/pnglist.txt" -mf fps=10 -o svgs/video.webm -ovc lavc -lavcopts vcodec=acodec=vorbis:vcodec=libvpx -of lavf -lavfopts format=webm -ffourcc VP80
