7e14039b8d3f68d82b09e586fe0f00e66d144f57
Arne Babenhauserheide <bab@draketo.de>
2011-02-27 23:09:53 +0100

    position cost: QWERTZ Q only 10 instead of 20 (less of a stretch than the key right of z).

--- config.py
+++ config.py
@@ -135,7 +135,7 @@ COST_PER_KEY  = [
     # The 0 values aren’t filled in at the moment.
     # Don’t put mutated keys there, otherwise the best keys will end up there!
     [50,    40,35,30,30, 35,   40,35,30,30,30,35,40,50], # Zahlenreihe (0)
-    [24,    20, 6, 4, 6, 9,    10, 6, 4, 5, 8,8,36, 0], # Reihe 1
+    [24,    10, 6, 4, 6, 9,    10, 6, 4, 5, 8,8,36, 0], # Reihe 1
     [5,      3, 3, 3, 3, 5,     5, 3, 3, 3, 3, 5,10,18], # Reihe 2
     [15,10, 16,24,20, 5,    30, 6, 5,22,22,11,      15],     # Reihe 3
     [0,0,0,               3           , 7, 0, 0, 0] # Reihe 4 mit Leertaste
