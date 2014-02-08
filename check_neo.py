e632a2d2a63384939a9fa51f87ac6345c7b1c49c
Arne Babenhauserheide <bab@draketo.de>
2014-02-08 01:14:27 +0100

    added comparisons to capewell and workman.

--- check_neo.py
+++ check_neo.py
@@ -571,6 +571,10 @@ def compare_a_layout(quiet, verbose, datafile=None, layout=NEO_LAYOUT, fingersta
         print_layout_with_statistics(DVORAK_LAYOUT, letters=letters, repeats=repeats, number_of_letters=datalen1, number_of_bigrams=datalen2, trigrams=trigrams, number_of_trigrams=number_of_trigrams, verbose=verbose, shorten_numbers=True, fingerstats=fingerstats)
         info("\nAnd Colemak")
         print_layout_with_statistics(COLEMAK_LAYOUT, letters=letters, repeats=repeats, number_of_letters=datalen1, number_of_bigrams=datalen2, trigrams=trigrams, number_of_trigrams=number_of_trigrams, verbose=verbose, shorten_numbers=True, fingerstats=fingerstats)
+        info("\nAnd Workman")
+        print_layout_with_statistics(WORKMAN_LAYOUT, letters=letters, repeats=repeats, number_of_letters=datalen1, number_of_bigrams=datalen2, trigrams=trigrams, number_of_trigrams=number_of_trigrams, verbose=verbose, shorten_numbers=True, fingerstats=fingerstats)
+        info("\nAnd Capewell")
+        print_layout_with_statistics(CAPEWELL_LAYOUT, letters=letters, repeats=repeats, number_of_letters=datalen1, number_of_bigrams=datalen2, trigrams=trigrams, number_of_trigrams=number_of_trigrams, verbose=verbose, shorten_numbers=True, fingerstats=fingerstats)
 
 # for compatibility
 check_the_neo_layout = compare_a_layout
