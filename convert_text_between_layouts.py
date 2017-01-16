3aca57ee602366cbe64c39f98c76308ba81bc797
Arne Babenhauserheide <arne.babenhauserheide@kit.edu>
2017-01-16 22:20:04 +0100

    convert_text_between_layouts.py: add cry and fix haeik and ghei

--- convert_text_between_layouts.py
+++ convert_text_between_layouts.py
@@ -176,14 +176,20 @@ heaiu dtrnmf
 haeik = """
 xyoü, pclmvß´
 haeik dtnrsw
-zäöu. bgjqf
-"""
+⇚zäöu. bgjqf
+"""[1:]
 
 ghei = """
 juo,ö qpmlvzß
 ghei. ctnrs⇘
-kyüaä bdwxf
-"""
+⇚kyüaä bdwxf
+"""[1:]
+
+cry = """
+qmuaz kdflvjß
+criey ptsnh⇘
+⇚xäüoö wg,.'
+"""[1:]
 
 #: example text. Values by textcheck in rev 64358e0a2d00: 173.896062828 (0.21715925231699246, 0.43412613860259547, 0.7601613230513935)
 text = """Wir waren nur noch ein paar Kilometer von dem anderen Tempel entfernt, als die Schatten aus dem Himmel zu fallen begannen, sich als Protektoren herausstellten und damit meine schlimmsten Befürchtungen bestätigten."""
